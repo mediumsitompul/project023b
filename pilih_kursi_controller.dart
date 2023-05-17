2feac32c3534077ee9609df714497de500681972
mediumsitompul <44793858+mediumsitompul@users.noreply.github.com>
2023-05-17 13:32:53 +0700

    Create pilih_kursi_controller.dart

---
+++ pilih_kursi_controller.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+
+class PilihKursiController extends GetxController {
+  //TODO: Implement PilihKursiController
+
+  var indexGerbong = 0.obs;
+
+  void gantiGerbong(int indexGerbongTerpilih) {
+    indexGerbong.value = indexGerbongTerpilih;
+    gerbong.refresh();
+  }
+
+  var gerbong = List.generate(
+      6,
+      (indexG) => List<Map<String, dynamic>>.generate(80, (indexK) {
+            if (indexG == 0) {
+              if (indexK >= 4 && indexK <= 5 || indexK >= 8 && indexK <= 9) {
+                return {
+                  "id": "ID-${indexG + 1}-${indexK + 1}",
+                  "status": "occupied",
+                };
+              }
+            } else if (indexG == 1) {
+              if (indexK >= 14 && indexK <= 15 ||
+                  indexK >= 18 && indexK <= 19) {
+                return {
+                  "id": "ID-${indexG + 1}-${indexK + 1}",
+                  "status": "occupied",
+                };
+              }
+            }
+
+            return {
+              "id": "ID-${indexG + 1}-${indexK + 1}",
+              "status": "available",
+            };
+          })).obs;
+}
